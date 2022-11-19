c378b4733050f6e99c5fbfe47ea34c1e2e60b71f
정진하 <jhjeong@whatap.io>
2022-11-19 20:13:26 +0900

    html in javascript

---
+++ momentum/app.js
@@ -0,0 +1,7 @@
+// document is the starting point of everything
+document.title = "Hello! From JS!";
+// grabing element...!
+const title = document.getElementById("title");
+console.dir(title);
+title.innerText = "Got You";
+console.log(title.className);
