5f9a39e9db08f9b707ab2f253021e392d7aa2356
jinha <jinha4ever@gmail.com>
2022-11-20 19:21:41 +0900

    background.js 주석 추가

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -3,6 +3,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 // Create HTML from java script
 const backgroundImage = document.createElement("img");
-
+// property에 값 넣어줌.
 backgroundImage.src = `img/${chosenImage}`;
+// body에 어펜드 하면 뒤에 붙이게 된다.
 document.body.appendChild(backgroundImage);
